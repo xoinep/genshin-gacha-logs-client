cbd9a49283d5fe4a62c813551bc3e7d6ed63b0ab
hoangnb <hoangnb@gearinc.com>
2021-02-27 20:28:55 +0700

    Updates

--- src/store/index.js
+++
@@ -1,6 +0,0 @@
-import { createStore } from "easy-peasy";
-import model from "./model";
-
-const store = createStore({ model });
-
-export default store;
--- src/store/model.js
+++
@@ -1,5 +0,0 @@
-import { action, thunk } from "easy-peasy";
-
-const model = {};
-
-export default model;
